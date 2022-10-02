ed147c79154b0b0815a8297da207e8ac2e07950d
minhaneul <onlynforever@gmail.com>
2022-10-02 16:42:42 +0900

    Add: CSV 파싱, 캐시 구현

--- lib/data/repository/stock_repository_impl.dart
+++ lib/data/repository/stock_repository_impl.dart
@@ -2,36 +2,40 @@ import '../../data/mapper/company_mapper.dart';
 import '../../domain/model/company_listing.dart';
 import '../../domain/repository/stock_repository.dart';
 import '../../util/result.dart';
+import '../csv/company_listing_parser.dart';
 import '../source/local/stock_dao.dart';
 import '../source/remote/stock_api.dart';
 
 class StockRepositoryImpl implements StockRepository {
   final StockApi _api;
   final StockDao _dao;
+  final _parser = CompanyListingsParser();
 
   StockRepositoryImpl(this._api, this._dao);
 
   @override
   Future<Result<List<CompanyListing>>> getCompanyListings(
       bool fetchFromRemote, String query) async {
-    // 캐시에서 찾는다
     final localListings = await _dao.searchCompanyListing(query);
-
-    // 없다면 리모트에서 가져온다
     final isDbEmpty = localListings.isEmpty && query.isEmpty;
     final shouldJustLoadFromCache = !isDbEmpty && !fetchFromRemote;
 
-    // 캐시
     if (shouldJustLoadFromCache) {
       return Result.success(
           localListings.map((e) => e.toCompanyListing()).toList());
     }
 
-    // 리모트
     try {
-      final remoteListings = await _api.getListings();
-      // TODO : CSV 파싱 변환
-      return const Result.success([]);
+      final response = await _api.getListings();
+      final remoteListings = await _parser.parse(response.body);
+
+      await _dao.clearCompanyListings();
+
+      await _dao.insertCompanyListings(
+          remoteListings.map((e) => e.toCompanyListingEntity()).toList()
+      );
+
+      return Result.success(remoteListings);
     } catch (e) {
       return Result.error(Exception('데이터 로드 실패!!'));
     }
